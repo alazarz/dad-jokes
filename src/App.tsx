fe97ebb84ea9dee7e01b7b3186824201d100c7a8
Alazar Zewde <ayzewde@gmail.com>
2021-12-12 18:09:36 -0600

    add search button with modal. modal renders successfully with test data

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import { AppBar, Typography, TextField, Grid, Button } from '@mui/material'
+import { makeStyles } from '@mui/styles'
 import Page from './components/Page/Page'
+import Modal from './components/Modal/Modal'
 
-const onSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
-  let searchParams = e.currentTarget.value;
-  console.log(searchParams)
-  //do logic for search through json for best joke
-  // set modal state to open and pass in joke string  
-}
+const useStyles: Function = makeStyles(() => ({
+  appBar: {
+    alignItems: 'center'
+  },
+  title: {
+    flexGrow: 1,
+    padding: 10
+  },
+  searchBarContainer: {
 
-const onTextChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-  let text = e.currentTarget.value;
-  console.log(text)
+  },
+  searchBar: {
 
-}
+  },
+  submitBtn: {
+
+  }
+}));
 
 
-const App = () => { 
+const App = () => {
+
+  //State
+  const [searchParams, setSearchParams] = useState<string>('')
+  const [searchResult, setSearchResult] = useState<string>('')
+  const [searchResultsOpen, setSearchResultsOpen] = useState<boolean>(false)
+  
+  //Handlers
+  const onSearch: VoidFunction = () => {
+    
+    //do logic for search through json for best joke
+    // set modal state to open and pass in joke string 
+    
+    setSearchResult("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    setSearchResultsOpen(true)
+  }
+
+  const onTextChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchParams(e.currentTarget.value);
+  }
+
+  const onModalClosed = () => setSearchResultsOpen(false)
+
+
+  const classes = useStyles()
   return (
-      <>
-      <Page title="Dad Jokes">
-      <AppBar position='static'>
-          <Typography variant='h4'>
-            Hello Dad Jokes World
+    <>
+      <AppBar position='static' className={classes.appBar}>
+          <Typography variant='h3' className={classes.title}>
+            The #1 Dad Joke Generator
           </Typography>
         </AppBar>
-        <Grid>
-          <TextField
-            variant='outlined'
-            onChange={onTextChanged}
-
+        <Page title='Enter your search term'>      
+          <Grid className={classes.searchBarContainer}>
+            <TextField
+              className={classes.searchBar}
+              variant='outlined'
+              onChange={onTextChanged}
+            />
+          </Grid>
+          <Grid>
+            <Button 
+              color='primary'
+              variant='contained'
+              onClick={onSearch}>
+                Search
+              </Button>
+          </Grid>
+          <Modal
+            isOpen={searchResultsOpen}
+            title='Search Results'
+            content={searchResult}
+            onClose={onModalClosed} 
           />
-
-        </Grid>
-      </Page>
+        </Page>
       </>
   );
 }
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,11 +1,32 @@
 import React, { FC } from 'react'
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material'
+import { Button } from '@mui/material'
 
 export interface IModalProps {
-
+    isOpen: boolean,
+    title: string,
+    content: string,
+    onClose: () => void
 }
 
-const Modal: FC<IModalProps> = () => {
-    return (<></>)
+const Modal: FC<IModalProps> = ({ isOpen, title, content, onClose }) => {
+    return (
+        <Dialog open={isOpen}>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    {content}
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    color='primary'
+                    onClick={onClose}>
+                        Close
+                    </Button>
+            </DialogActions>
+        </Dialog>
+    )
 }
 
 export default Modal
